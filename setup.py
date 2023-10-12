92fadd1480197e64bcce401478714fdbd74fa6b5
carlosP1305 <carlos.pacheco@kemok.io>
2023-10-12 11:26:57 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.7' 
+VERSION = '0.0.8' 
 DESCRIPTION = 'Paquete de notificaciones kemok'
 LONG_DESCRIPTION = 'Paquete de canales de notificaciones de kemok'
 
